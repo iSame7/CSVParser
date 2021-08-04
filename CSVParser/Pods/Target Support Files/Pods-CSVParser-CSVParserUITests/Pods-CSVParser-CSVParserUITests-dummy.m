bceef69f60f3e3b98a295a2e87dcec7431f19ce3
Sameh Mabrouk <mabrouksameh@gmail.com>
2021-08-04 13:59:47 +0200

    Setup cocoapods

---
+++ CSVParser/Pods/Target Support Files/Pods-CSVParser-CSVParserUITests/Pods-CSVParser-CSVParserUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CSVParser_CSVParserUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CSVParser_CSVParserUITests
+@end
---
+++ CSVParser/Pods/Target Support Files/Pods-CSVParser/Pods-CSVParser-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CSVParser : NSObject
+@end
+@implementation PodsDummy_Pods_CSVParser
+@end

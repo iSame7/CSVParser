7415ca3bb910136136e725f9abfbdd2419ac974f
Sameh Mabrouk <mabrouksameh@gmail.com>
2021-08-04 14:05:48 +0200

    Create CSVParserKit

---
+++ CSVParser/Common/CSVParserKit/CSVParserKit/CSVParserKit.h
@@ -0,0 +1,18 @@
+//
+//  CSVParserKit.h
+//  CSVParserKit
+//
+//  Created by Sameh Mabrouk on 04/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CSVParserKit.
+FOUNDATION_EXPORT double CSVParserKitVersionNumber;
+
+//! Project version string for CSVParserKit.
+FOUNDATION_EXPORT const unsigned char CSVParserKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSVParserKit/PublicHeader.h>
+
+
